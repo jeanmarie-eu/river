8eb65db87b187649b74e71d88997de6ec16c8ec6
jeanmarielepioufle <jm@nexmodeling.com>
2017-04-04 11:19:55 +0200

    made public

---
+++ R/river_norway.R
@@ -0,0 +1,12 @@
+#' Norwegian rivers
+#'
+#' River shapefile convert into R format.
+#' The original dataset is available at http://nedlasting.nve.no/gis/
+#'
+#' @docType data
+#' @name river_norway
+#' @format A rda file
+#' @keywords datasets
+#' @references the Norwegian Water Resources and Energy Directorate (NVE)
+#' @source \href{http://www.nve.no}{NVE}
+NULL
